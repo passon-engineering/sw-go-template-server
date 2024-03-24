0cc6ffc62d6a978c0cf8f5d9564a65d6580d9145
passont <mail@thomaspasson.de>
2024-03-24 16:20:46 +0100

    Included redirect error message also to logger module

--- modules/webserver/handleRedirectToHttps.go
+++ modules/webserver/handleRedirectToHttps.go
@@ -1,22 +1,24 @@
 package webserver
 
 import (
-	"log"
 	"net/http"
 	"sw-go-template-server/modules/application"
+	"time"
 
 	"github.com/passon-engineering/sw-go-logger-lib/logger"
 )
 
 func handleRedirectToHttps(app *application.Application) {
+	startTime := time.Now()
 	redirectServer := &http.Server{
 		Addr: app.Config.HttpAddress,
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 			app.Logger.Entry(logger.Container{
-				Status:      logger.STATUS_INFO,
-				Source:      "redirectToHTTPS",
-				HttpRequest: r,
+				Status:         logger.STATUS_INFO,
+				Source:         "handleRedirectToHttps",
+				HttpRequest:    r,
+				ProcessingTime: time.Since(startTime),
 			})
 
 			u := "https://" + app.Config.HttpsAddress + r.RequestURI
@@ -26,7 +28,12 @@ func handleRedirectToHttps(app *application.Application) {
 
 	go func() {
 		if err := redirectServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Error initializing redirect server: %s", err)
+			app.Logger.Entry(logger.Container{
+				Status:         logger.STATUS_ERROR,
+				Source:         "handleRedirectToHttps",
+				Info:           "could not redirect: " + err.Error(),
+				ProcessingTime: time.Since(startTime),
+			})
 			return
 		}
 	}()
